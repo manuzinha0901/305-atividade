ca3d08bf4937390353bbdf8d52ff2daebd672125
emanuelle <emanuelle.sanntoslima@gmail.com>
2019-07-19 17:04:05 -0300

    adicionar sistema dos dias da semana

--- script.js
+++ script.js
@@ -28,5 +28,49 @@ else if (idade >0 && idade <=12){
 }   
 
 else{
-     console.log("erro")}
+    console.log("erro")}
+
+let numeros = Number(prompt("Digite um numero de 1 a 7 "));
+console.log()
+
+let n1 = 1; 
+let n2 = 2;
+let n3 = 3;
+let n4 = 4;
+let n5 = 5;
+let n6 = 6;
+let n7 = 7;
+
+if(numeros === 1){
+    console.log(`o dia ${n1} é domingo`);
+}    
+else if  (numeros === 2){
+console.log(`o dia ${n2} é segunda-feira`);
+}
+
+else if  (numeros === 3){
+    console.log(`o dia ${n3} é terça-feira`);
+}
+
+else if  (numeros === 4){
+    console.log(`o dia ${n4} é quarta-feira`);
+}
+
+else if  (numeros === 5){
+    console.log(`o dia ${n5} é quinta-feira`);
+}
+else if  (numeros === 6){
+    console.log(`o dia ${n6} é sexta-feira`);
+}
+else if  (numeros === 7){
+    console.log(`o dia ${n7} é sábado `);
+}
+
+else {
+    console.log("ERRO")}
+
+
+
+
+
 
